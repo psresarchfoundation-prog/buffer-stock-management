0ee1fe082ef4431d66af0903958546e9ac7de79c
psresarchfoundation-prog <ps.resarchfoundation@gmail.com>
2026-01-01 17:58:02 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,10 +1,10 @@
 USERS = {
     "TSD": {
-        "password": "TSD@123",
+        "password": "TSDwh@2026",
         "role": "TSD"
     },
     "HOD": {
-        "password": "HOD@123",
+        "password": "HODwh@2026",
         "role": "HOD"
     }
 }
@@ -13,3 +13,4 @@ def authenticate(username, password):
     if username in USERS and USERS[username]["password"] == password:
         return True, USERS[username]["role"]
     return False, None
+
